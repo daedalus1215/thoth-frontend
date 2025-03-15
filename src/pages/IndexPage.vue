d81502c9d6553b0cc3740230f533e2a26d07fe4b
la la <13687739+daedalus1215@users.noreply.github.com>
2025-03-14 21:11:35 -0400

    We can now run the app

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <AudioRecorder />
+  </q-page>
+</template>
+
+<script>
+import AudioRecorder from 'components/AudioRecorder/AudioRecorder.vue'
+
+export default {
+  components: {
+    AudioRecorder,
+  },
+}
+</script>
